25c99e44e5beb4a64bcfe69650ab0eae0f327bdc
yumi-rakus <yumi.takahashi@rakus-partners.co.jp>
2020-07-03 12:19:10 +0900

    AdministratorController.javaのlogin()でsessionスコープにログインする管理者の名前を格納する処理を追加

--- src/main/java/jp/co/sample/emp_management/controller/AdministratorController.java
+++ src/main/java/jp/co/sample/emp_management/controller/AdministratorController.java
@@ -127,6 +127,9 @@ public class AdministratorController {
 			model.addAttribute("errorMessage", "メールアドレスまたはパスワードが不正です。");
 			return toLogin();
 		}
+		
+		session.setAttribute("administratorName", administrator.getName());
+		
 		return "forward:/employee/showList";
 	}
 
