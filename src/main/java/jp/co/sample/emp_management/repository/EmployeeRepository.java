8e367acff37f6274f4e6be0bd0bfa647530de452
yumi-rakus <yumi.takahashi@rakus-partners.co.jp>
2020-07-03 14:32:29 +0900

    EmployeeRepository.javaの従業員詳細を取得するload()のSQL文SELECT句にkananameを追加

--- src/main/java/jp/co/sample/emp_management/repository/EmployeeRepository.java
+++ src/main/java/jp/co/sample/emp_management/repository/EmployeeRepository.java
@@ -66,7 +66,7 @@ public class EmployeeRepository {
 	 * @exception 従業員が存在しない場合は例外を発生します
 	 */
 	public Employee load(Integer id) {
-		String sql = "SELECT id,name,image,gender,hire_date,mail_address,zip_code,address,telephone,salary,characteristics,dependents_count FROM employees WHERE id=:id";
+		String sql = "SELECT id,name,image,gender,hire_date,mail_address,zip_code,address,telephone,salary,characteristics,dependents_count, kananame FROM employees WHERE id=:id";
 
 		SqlParameterSource param = new MapSqlParameterSource().addValue("id", id);
 
