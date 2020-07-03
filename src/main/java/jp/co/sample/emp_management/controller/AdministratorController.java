3e56a0f8ec99a4cb6aec29beca183c84efcec83b
yumi-rakus <yumi.takahashi@rakus-partners.co.jp>
2020-07-03 11:59:46 +0900

    AdministratorController.javaのinsert()にダブルサブミット対策としてredirectに修正

--- src/main/java/jp/co/sample/emp_management/controller/AdministratorController.java
+++ src/main/java/jp/co/sample/emp_management/controller/AdministratorController.java
@@ -88,8 +88,11 @@ public class AdministratorController {
 		// フォームからドメインにプロパティ値をコピー
 		BeanUtils.copyProperties(form, administrator);
 		administratorService.insert(administrator);
-		return "administrator/login";
+		
+		return "redirect:/";
 	}
+	
+	
 
 	/////////////////////////////////////////////////////
 	// ユースケース：ログインをする
