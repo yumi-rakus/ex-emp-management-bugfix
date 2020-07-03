979ed317b1bcaa46d750811355a13d9c0332c158
yumi-rakus <yumi.takahashi@rakus-partners.co.jp>
2020-07-03 16:16:06 +0900

    EmployeeController.javaに従業員情報を曖昧検索するsearch()を追加、検索結果を表示するshowSearchResult()を追加

--- src/main/java/jp/co/sample/emp_management/controller/EmployeeController.java
+++ src/main/java/jp/co/sample/emp_management/controller/EmployeeController.java
@@ -9,6 +9,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import jp.co.sample.emp_management.domain.Employee;
 import jp.co.sample.emp_management.form.UpdateEmployeeForm;
@@ -17,7 +18,7 @@ import jp.co.sample.emp_management.service.EmployeeService;
 /**
  * 従業員情報を操作するコントローラー.
  * 
- * @author igamasayuki
+ * @author yumi takahashi
  *
  */
 @Controller
@@ -26,7 +27,7 @@ public class EmployeeController {
 
 	@Autowired
 	private EmployeeService employeeService;
-	
+
 	/**
 	 * 使用するフォームオブジェクトをリクエストスコープに格納する.
 	 * 
@@ -53,14 +54,13 @@ public class EmployeeController {
 		return "employee/list";
 	}
 
-	
 	/////////////////////////////////////////////////////
 	// ユースケース：従業員詳細を表示する
 	/////////////////////////////////////////////////////
 	/**
 	 * 従業員詳細画面を出力します.
 	 * 
-	 * @param id リクエストパラメータで送られてくる従業員ID
+	 * @param id    リクエストパラメータで送られてくる従業員ID
 	 * @param model モデル
 	 * @return 従業員詳細画面
 	 */
@@ -70,20 +70,19 @@ public class EmployeeController {
 		model.addAttribute("employee", employee);
 		return "employee/detail";
 	}
-	
+
 	/////////////////////////////////////////////////////
 	// ユースケース：従業員詳細を更新する
 	/////////////////////////////////////////////////////
 	/**
 	 * 従業員詳細(ここでは扶養人数のみ)を更新します.
 	 * 
-	 * @param form
-	 *            従業員情報用フォーム
+	 * @param form 従業員情報用フォーム
 	 * @return 従業員一覧画面へリダクレクト
 	 */
 	@RequestMapping("/update")
 	public String update(@Validated UpdateEmployeeForm form, BindingResult result, Model model) {
-		if(result.hasErrors()) {
+		if (result.hasErrors()) {
 			return showDetail(form.getId(), model);
 		}
 		Employee employee = new Employee();
@@ -91,5 +90,54 @@ public class EmployeeController {
 		employee.setDependentsCount(form.getIntDependentsCount());
 		employeeService.update(employee);
 		return "redirect:/employee/showList";
+	}
+
+	/////////////////////////////////////////////////////
+	// ユースケース：従業員を検索する
+	/////////////////////////////////////////////////////
+	/**
+	 * 検索された従業員を表示します.
+	 * 
+	 * @param keyName            曖昧検索キー
+	 * @param redirectAttributes Flashスコープ
+	 * @return 検索された従業員一覧画面へリダイレクト
+	 */
+	@RequestMapping("/search")
+	public String search(String keyName, RedirectAttributes redirectAttributes) {
+
+		List<Employee> employeeList = employeeService.search(keyName);
+
+		// 空文字で検索した場合全件検索結果を表示
+		if (keyName.trim().equals("")) {
+			employeeList = employeeService.showList();
+			redirectAttributes.addFlashAttribute("employeeList", employeeList);
+
+			return "redirect:/employee/showSearchResult";
+		}
+
+		// 指定した文字列が存在しなかった場合「１件もありませんでした」というメッセージと共に全件検索結果を表示
+		if (employeeList.isEmpty()) {
+
+			employeeList = employeeService.showList();
+
+			redirectAttributes.addFlashAttribute("result", "お探しの従業員は1件も存在しませんでした");
+			redirectAttributes.addFlashAttribute("employeeList", employeeList);
+
+			return "redirect:/employee/showSearchResult";
+		}
+
+		redirectAttributes.addFlashAttribute("employeeList", employeeList);
+
+		return "redirect:/employee/showSearchResult";
+	}
+
+	/**
+	 * 検索された従業員一覧画面を出力します.
+	 * 
+	 * @return 検索された従業員一覧画面
+	 */
+	@RequestMapping("/showSearchResult")
+	public String showSearchResult() {
+		return "employee/list";
 	}
 }
