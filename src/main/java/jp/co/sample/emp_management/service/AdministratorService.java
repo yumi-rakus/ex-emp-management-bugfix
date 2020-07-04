7a34d6feae28591111e09102f3ad17e142465d21
yumi-rakus <yumi.takahashi@rakus-partners.co.jp>
2020-07-04 17:10:12 +0900

    AdministratorService.javaのinsert()でハッシュ化したパスワードをDBにinsertするように修正

--- src/main/java/jp/co/sample/emp_management/service/AdministratorService.java
+++ src/main/java/jp/co/sample/emp_management/service/AdministratorService.java
@@ -1,6 +1,7 @@
 package jp.co.sample.emp_management.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -10,40 +11,50 @@ import jp.co.sample.emp_management.repository.AdministratorRepository;
 /**
  * 管理者情報を操作するサービス.
  * 
- * @author igamasayuki
+ * @author yumi takahashi
  *
  */
 @Service
 @Transactional
 public class AdministratorService {
-	
+
 	@Autowired
 	private AdministratorRepository administratorRepository;
+
+	@Autowired
+	private PasswordEncoder passwordEncoder;
 
 	/**
 	 * 管理者情報を登録します.
 	 * 
-	 * @param administrator　管理者情報
+	 * @param administrator 管理者情報
 	 */
-	public void insert(Administrator administrator) {
+	public void insert(Administrator administrator, String rawPassword) {
+
+		String encodedPassword = passwordEncoder.encode(rawPassword);
+
+		administrator.setPassword(encodedPassword);
+
 		administratorRepository.insert(administrator);
 	}
-	
+
 	/**
 	 * ログインをします.
+	 * 
 	 * @param mailAddress メールアドレス
-	 * @param password パスワード
-	 * @return 管理者情報　存在しない場合はnullが返ります
+	 * @param password    パスワード
+	 * @return 管理者情報 存在しない場合はnullが返ります
 	 */
 	public Administrator login(String mailAddress, String passward) {
 		Administrator administrator = administratorRepository.findByMailAddressAndPassward(mailAddress, passward);
 		return administrator;
 	}
-	
+
 	/**
 	 * メールアドレスが既に登録されているかを確認します.
+	 * 
 	 * @param mailAddress メールアドレス
-	 * @return　管理者情報 存在しない場合はnullが返ります
+	 * @return 管理者情報 存在しない場合はnullが返ります
 	 */
 	public Administrator findByMailAddress(String mailAddress) {
 		return administratorRepository.findByMailAddress(mailAddress);
