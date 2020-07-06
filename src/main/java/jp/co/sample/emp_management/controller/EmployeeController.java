5903c4d54b64777530ad7c50d978ace2231b25be
yumi-rakus <yumi.takahashi@rakus-partners.co.jp>
2020-07-06 17:40:19 +0900

    EmployeeController.javaに従業員登録のためのフォームをrequestスコープに格納するsetUpInsertEmployeeForm()、従業員新規登録画面に遷移するtoRegister()、従業員を新規登録するregister()を追加

--- src/main/java/jp/co/sample/emp_management/controller/EmployeeController.java
+++ src/main/java/jp/co/sample/emp_management/controller/EmployeeController.java
@@ -1,9 +1,13 @@
 package jp.co.sample.emp_management.controller;
 
+import java.sql.Date;
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.http.HttpSession;
 
+import org.apache.tomcat.util.codec.binary.Base64;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import jp.co.sample.emp_management.domain.Employee;
+import jp.co.sample.emp_management.form.InsertEmployeeForm;
 import jp.co.sample.emp_management.form.UpdateEmployeeForm;
 import jp.co.sample.emp_management.service.EmployeeService;
 
@@ -41,6 +46,11 @@ public class EmployeeController {
 	@ModelAttribute
 	public UpdateEmployeeForm setUpForm() {
 		return new UpdateEmployeeForm();
+	}
+
+	@ModelAttribute
+	public InsertEmployeeForm setUpInsertEmployeeForm() {
+		return new InsertEmployeeForm();
 	}
 
 	/////////////////////////////////////////////////////
@@ -188,4 +198,112 @@ public class EmployeeController {
 	public String showSearchResult() {
 		return "employee/list";
 	}
+
+	/////////////////////////////////////////////////////
+	// ユースケース：従業員を新規登録する
+	/////////////////////////////////////////////////////
+	/**
+	 * 従業員新規登録画面を出力します.
+	 * 
+	 * @param model モデル
+	 * @return 従業員新規登録画面
+	 */
+	@RequestMapping("/toRegister")
+	public String toRegister(Model model) {
+
+		// hireYear(1900~現在の西暦)
+		List<Object> hireYearList = new ArrayList<>();
+
+		LocalDate ldt = LocalDate.now();
+		Integer currentYear = ldt.getYear();
+
+		hireYearList.add("----");
+
+		for (Integer i = currentYear; i >= 1900; i--) {
+			hireYearList.add(i);
+		}
+
+		model.addAttribute("hireYearList", hireYearList);
+
+		// hireMonth(1~12)
+		List<Object> hireMonthList = new ArrayList<>();
+
+		hireMonthList.add("----");
+
+		for (Integer i = 1; i <= 12; i++) {
+			hireMonthList.add(i);
+		}
+
+		model.addAttribute("hireMonthList", hireMonthList);
+
+		// hireDay(1~31)
+		List<Object> hireDayList = new ArrayList<>();
+
+		hireDayList.add("----");
+
+		for (Integer i = 1; i <= 31; i++) {
+			hireDayList.add(i);
+		}
+
+		model.addAttribute("hireDayList", hireDayList);
+
+		return "employee/register";
+	}
+
+	/**
+	 * 従業員を新規登録します.
+	 * 
+	 * @param form   フォーム
+	 * @param result BindingResultオブジェクト
+	 * @param model  モデル
+	 * @return 従業員一覧画面
+	 * @throws Exception
+	 */
+	@RequestMapping("/register")
+	synchronized String register(@Validated InsertEmployeeForm form, BindingResult result, Model model)
+			throws Exception {
+
+		if (result.hasErrors()) {
+			return toRegister(model);
+		}
+
+		System.out.println(form.getImage().getSize());
+
+		StringBuffer data = new StringBuffer();
+
+		String base64 = new String(Base64.encodeBase64(form.getImage().getBytes()), "ASCII");
+
+		data.append("data:image/jpeg;base64,");
+		data.append(base64);
+
+		String imageData = data.toString();
+
+		Employee employee = new Employee();
+
+		employee.setName(form.getName());
+		employee.setKanaName(form.getKanaName());
+		employee.setImage(imageData);
+		employee.setGender(form.getGender());
+
+		LocalDate hireDateL = LocalDate.of(Integer.parseInt(form.getHireYear()), Integer.parseInt(form.getHireMonth()),
+				Integer.parseInt(form.getHireDay()));
+
+		Date hireDate = Date.valueOf(hireDateL);
+
+		employee.setHireDate(hireDate);
+
+		employee.setMailAddress(form.getMailAddress());
+		employee.setZipCode(form.getZipCodeFirst() + "-" + form.getZipCodeLast());
+		employee.setAddress(form.getAddressFirst() + form.getAddressLast());
+		employee.setTelephone(
+				form.getTelephoneFirst() + "-" + form.getTelephoneMiddle() + "-" + form.getTelephoneLast());
+		employee.setSalary(form.getSalary());
+		employee.setCharacteristics(form.getCharacteristics());
+		employee.setDependentsCount(form.getDependentsCount());
+
+		employeeService.register(employee);
+
+		return "redirect:/employee/showList";
+	}
+
 }
