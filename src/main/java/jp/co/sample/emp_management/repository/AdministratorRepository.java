d4d882055df81e5a0176a8e33b911a8e0957682f
yumi-rakus <yumi.takahashi@rakus-partners.co.jp>
2020-07-10 19:45:18 +0900

    AdministratorRepository.javaのfindByMailAddressAndPassword()を削除

--- src/main/java/jp/co/sample/emp_management/repository/AdministratorRepository.java
+++ src/main/java/jp/co/sample/emp_management/repository/AdministratorRepository.java
@@ -49,23 +49,6 @@ public class AdministratorRepository {
 		Administrator administrator = template.queryForObject(sql, param, ADMINISTRATOR_ROW_MAPPER);
 		return administrator;
 	}
-	
-	/**
-	 * メールアドレスとパスワードから管理者情報を取得します.
-	 * 
-	 * @param mailAddress メールアドレス
-	 * @param passward    パスワード
-	 * @return 管理者情報 存在しない場合はnullを返します
-	 */
-	public Administrator findByMailAddressAndPassward(String mailAddress, String password) {
-		String sql = "select id,name,mail_address,password from administrators where mail_address= :mailAddress and password= :password";
-		SqlParameterSource param = new MapSqlParameterSource().addValue("mailAddress", mailAddress).addValue("password", password);
-		List<Administrator> administratorList = template.query(sql, param, ADMINISTRATOR_ROW_MAPPER);
-		if (administratorList.size() == 0) {
-			return null;
-		}
-		return administratorList.get(0);
-	}
 
 	/**
 	 * 管理者情報を挿入します.
