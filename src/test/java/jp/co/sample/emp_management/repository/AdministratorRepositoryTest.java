89ed7f8930821e5df7a91f3c3517b7d3f5575ad9
yumi-rakus <yumi.takahashi@rakus-partners.co.jp>
2020-07-10 19:42:42 +0900

    AdministratorRepositoryTest.javaを修正

--- src/test/java/jp/co/sample/emp_management/repository/AdministratorRepositoryTest.java
+++ src/test/java/jp/co/sample/emp_management/repository/AdministratorRepositoryTest.java
@@ -1,6 +1,7 @@
 package jp.co.sample.emp_management.repository;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -10,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import jp.co.sample.emp_management.domain.Administrator;
@@ -23,14 +25,18 @@ class AdministratorRepositoryTest {
 
 	@Autowired
 	private NamedParameterJdbcTemplate template;
+	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
 
+	// 付与されたメソッドは各テスト/インナーテストクラスが実行される前に実行される
 	@BeforeEach
 	public void testInsert() {
 		System.out.println("DB初期化処理開始");
 		Administrator administrator = new Administrator();
 		administrator.setName("伊賀将之");
 		administrator.setMailAddress("igaiga@sample.com");
-		administrator.setPassword("testtest");
+		administrator.setPassword(passwordEncoder.encode("testtest"));
 		administratorRepository.insert(administrator);
 		System.out.println("インサートが完了しました。");
 
@@ -47,24 +53,26 @@ class AdministratorRepositoryTest {
 
 		assertEquals("伊賀将之", resultAdministrator.getName(), "名前が登録されていません");
 		assertEquals("igaiga@sample.com", resultAdministrator.getMailAddress(), "メールアドレスが登録されていません");
-		assertEquals("testtest", resultAdministrator.getPassword(), "パスワードが登録されていません");
+		assertTrue(passwordEncoder.matches("testtest", resultAdministrator.getPassword()), "パスワードが登録されていません");
+		
 
 		System.out.println("主キー検索するテスト終了");
 	}
 
 	@Test
-	public void testFindByMailAddressAndPassward() {
-		System.out.println("メールアドレスとパスワードで検索するテスト開始");
-		Administrator resultAdministrator = administratorRepository.findByMailAddressAndPassward("igaiga@sample.com",
-				"testtest");
-
+	public void testFindByMailAddress() {
+		System.out.println("メールアドレスから従業員情報を取得するテスト開始");
+		
+		Administrator resultAdministrator = administratorRepository.findByMailAddress("igaiga@sample.com");
+		
 		assertEquals("伊賀将之", resultAdministrator.getName(), "名前が登録されていません");
 		assertEquals("igaiga@sample.com", resultAdministrator.getMailAddress(), "メールアドレスが登録されていません");
-		assertEquals("testtest", resultAdministrator.getPassword(), "パスワードが登録されていません");
-
-		System.out.println("メールアドレスとパスワードで検索するテスト終了");
+		assertTrue(passwordEncoder.matches("testtest", resultAdministrator.getPassword()), "パスワードが登録されていません");
+		
+		System.out.println("メールアドレスから従業員情報を取得するテスト終了");
 	}
 
+	// 付与されたメソッドは各テスト/インナーテストクラスが実行された後に実行される
 	@AfterEach
 	public void tearDownAfterClass() throws Exception {
 		MapSqlParameterSource param = new MapSqlParameterSource().addValue("mailAddress", "igaiga@sample.com");
